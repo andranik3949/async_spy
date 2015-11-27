551dceaa097bf94a68e3f1ff194ca7e3f1dcbd9a
andranik3949 <andranikmelikyan@gmail.com>
2015-11-27 19:49:52 +0400

    fixed converter waiting

--- async_spy/Converter.cs
+++ async_spy/Converter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace async_spy
 
         public static void convert()
         {
+            List<Task> taskList = new List<Task>();
             while (true)
             {
                 Filename current;
@@ -29,39 +31,22 @@ namespace async_spy
                     }
                     else
                     {
+                        Task.WaitAll(taskList.ToArray());
                         break;
                     }
                 }
                 if (URLGenerator.directories[current.dirNum - 1].getDownload())
                 {
                     m_threadFree.WaitOne();
-                    Task.Run(() =>
-                   {
-                       convert_file(current);
-                   }).ContinueWith((lastTask) => { m_threadFree.Release(); });
+                    taskList.Add ( Task.Run(() =>
+                    {
+                        convert_file(current);
+                    }).ContinueWith((lastTask) => { m_threadFree.Release(); }) );
                 }
                 else
                 {
                     Console.WriteLine("Skipping " + Config.local_xls_base + current.toXLS());
                 }
-
-                /*Console.WriteLine("Converting " + Config.local_xls_base + filename);
-                try
-                {
-                    Process conversion = new Process();
-                    conversion.StartInfo.FileName = @"C:\Program Files (x86)\Microsoft Office\Office12\excelcnv.exe";
-                    conversion.StartInfo.Arguments = string.Format(@" -nme -oice {0} {1}", Config.local_xls_base + filename, Config.local_xlsx_base + filename);
-                    conversion.Start();
-
-                    conversion.WaitForExit(1000 * 60);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Failed to convert " + filename);
-                    Console.WriteLine(ex.Message);
-                    throw ex;
-                };
-                Console.WriteLine(Config.local_xls_base + filename + " saved as " + Config.local_xlsx_base + filename);*/
             }
         }
 
@@ -71,7 +56,7 @@ namespace async_spy
             Console.WriteLine("Converting " + Config.local_xls_base + currXLS + " to " + Config.local_xlsx_base + current.toXLSX());
             try
             {
-                Thread.Sleep(100);
+                Thread.Sleep(333);
             }
             catch (Exception ex)
             {
@@ -80,8 +65,26 @@ namespace async_spy
                 //throw ex;
             }
             Console.WriteLine("Converted " + currXLS);
-        }
 
-        private static Semaphore m_threadFree;
+            /*Console.WriteLine("Converting " + Config.local_xls_base + filename);
+            try
+            {
+                Process conversion = new Process();
+                conversion.StartInfo.FileName = @"C:\Program Files (x86)\Microsoft Office\Office12\excelcnv.exe";
+                conversion.StartInfo.Arguments = string.Format(@" -nme -oice {0} {1}", Config.local_xls_base + filename, Config.local_xlsx_base + filename);
+                conversion.Start();
+
+                conversion.WaitForExit(1000 * 60);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to convert " + filename);
+                Console.WriteLine(ex.Message);
+                throw ex;
+            };
+            Console.WriteLine(Config.local_xls_base + filename + " saved as " + Config.local_xlsx_base + filename);*/
+         }
+
+         private static Semaphore m_threadFree;
     }
 }
